eb92a8667be823a691480994b41de34e3b498b97
Max Gomez <gmax0687@gmail.com>
2021-10-21 21:04:39 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+#DO THE FORENSICS QUESTIONS FIRST, THEN RUN THE SCRIPT
+1. updates
+    * settings => software and updates => updates
+    * check for updates daily, download and install automatically security updates
+2. firewall
+    * turn it on, reject incoming, allow outgoing
+3. malware and virus
+    * just use clamscan
+    * remove potentially malicious software(wireshark, jack the ripper, etc)
+4. groups
+    * just use the gui, its really annoying
+5. firefox settings
+    * disable login saving, block popup windows, warn when websites try to install addons, etc.
+
---
+++ script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+sudo apt update
+echo "updated packages"
+sleep 1
+#Install some packages
+sudo apt install gufw clamav libpam-cracklib neovim
+echo "Installed packages"
+sleep 1
+#Remove some packages
+sudo apt remove wireshark john
+echo "Removed potentialy malicious software"
+sleep 1
+#Disallow guest logins
+echo "allow-guest=false" | sudo tee -a /etc/lightdm/lightdm.conf
+echo "Dissalowed guest logins"
+sleep 1
+#Change some password policy
+sudo sed 's/^PASS_MAX_DAYS.*/PASS_MAX_DAYS   90/g' /etc/login.defs
+sudo sed 's/^PASS_MIN_DAYS.*/PASS_MIN_DAYS   10/g' /etc/login.defs
+sudo sed 's/^PASS_WARN_AGE.*/PASS_WARN_AGE   7/g' /etc/login.defs
+echo "auth required pam_tally2.so deny=5 onerr=fail unlock_time=1800" | sudo tee -a /etc/pam.d/common-auth
+echo "Change password policy"
+#Run an antivirus scan
+clamscan
+
